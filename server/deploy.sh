1c913977542cbeec405bd3fa2c09b7b01a89d3db
sadainiqrar <sadain.abbasi@gmail.com>
2019-04-08 22:49:58 +0500

    port setup

--- server/deploy.sh
+++ server/deploy.sh
@@ -5,9 +5,10 @@ echo "1/7) Compile sources ..."
 NODE_ENV=production webpack --config webpack.server.config.js --progress
 
 echo "2/7) Stopping existing node process ..."
-forever stop bincee
+forever stop control
 
 echo "3/7) Starting node process ..."
-NODE_ENV=production CONFIG=production forever start --uid "bincee" --append -c "node server/dist/build.js" .
+sudo lsof -t -i tcp:5000 -s tcp:listen | sudo xargs kill
+NODE_ENV=production CONFIG=production forever start --uid "control" --append -c "node server/dist/build.js" .
 
 echo "Deployment complete"
--- server/deploy.staging.sh
+++ server/deploy.staging.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -x
 
 echo "1/7) Compile sources ..."
+
 NODE_ENV=production webpack --config webpack.server.config.js --progress
 
 echo "2/7) Stopping existing node process ..."
-forever stop vizdev
+forever stop control
 
 echo "3/7) Starting node process ..."
-NODE_ENV=production CONFIG=staging forever start --uid "vizdev" --append -c "node server/dist/build.js" .
+sudo lsof -t -i tcp:5000 -s tcp:listen | sudo xargs kill
+NODE_ENV=production CONFIG=production forever start --uid "control" --append -c "node server/dist/build.js" .
 
 echo "Deployment complete"
